847c230ec727d5a02aeacb94b032a72f24b1a1cf
nmmyt <nmmyt@MINHMY>
2022-05-04 20:22:15 +0700

    Add project files.

---
+++ Navigation Drawer App/MainWindow.xaml.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Windows.Media.Animation;
+
+namespace Navigation_Drawer_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        Red_BlackTree tree = new Red_BlackTree();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_insert.Visibility = Visibility.Collapsed;
+                tt_remove.Visibility = Visibility.Collapsed;
+                tt_find.Visibility = Visibility.Collapsed;
+                tt_download.Visibility = Visibility.Collapsed;
+                tt_refresh.Visibility = Visibility.Collapsed;
+
+            }
+            else
+            
+            {
+                tt_insert.Visibility = Visibility.Visible;
+                tt_remove.Visibility = Visibility.Visible;
+                tt_find.Visibility = Visibility.Visible;
+                tt_download.Visibility = Visibility.Visible;
+                tt_refresh.Visibility = Visibility.Visible;
+
+            }
+        }
+
+        
+
+        private void insertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mainCanvas.Opacity = 0.3;
+
+        }
+        private void mainCanvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mainCanvas.Opacity = 0.3;
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (tree.Find(Convert.ToInt32(tb_delete.Text)) == null)
+                MessageBox.Show("Number is not exit");
+            else
+            {
+                tree(Convert.ToInt32(tb_delete.Text));
+            } 
+                
+            mainCanvas.Opacity = 1;
+        }
+        private void findBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mainCanvas.Opacity = 0.3;
+        }
+        private void btn_find_Click(object sender, RoutedEventArgs e)
+        {
+            double value;
+            if (!double.TryParse(tb_find.Text, out value))
+                MessageBox.Show("Number is not exit");
+            else if (tree.Find(Convert.ToInt32(tb_find.Text))==null)
+            {
+                MessageBox.Show("Find unseen");
+            }
+            
+                
+            mainCanvas.Opacity = 1;
+        }
+        private void refreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            mainCanvas.Children.Clear();
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            mainCanvas.Opacity = 0.3;
+        }
+
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            mainCanvas.Opacity = 1;
+        }
+
+
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+         
+            this.WindowState= WindowState.Minimized;
+        }
+        private void EnlargeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+
+
+        struct point
+        {
+            public double x, y;
+        }
+        static double x = 464, y = 20, count = 0, floor = 0,time=2 ;
+
+      
+
+        void addLine(double x1, double y1, double x2, double y2)
+        {
+            Line line_1 = new Line();
+            line_1.X1 = x1;
+            line_1.Y1 = y1;
+            line_1.X2 = x2;
+            line_1.Y2 = y2;
+            line_1.Stroke = System.Windows.Media.Brushes.Black;
+            line_1.StrokeThickness = 2;
+            mainCanvas.Children.Add(line_1);
+        }
+
+       
+
+        point find_point(int flo, int pos) // set toa do diem ve circle
+        {
+            point toado;
+            double distane = (928) / (flo * 2 + 1);
+            toado.x = (distane) * pos;
+            toado.y = y + 60 * flo;
+
+            return toado;
+        }
+
+       
+
+        private void btn_insert_Click(object sender, RoutedEventArgs e)
+        {
+            time = 4.1- 0.04 * slider.Value;
+           
+            double value = 0;
+            if (!double.TryParse(tb_insert.Text, out value))
+                MessageBox.Show("Insert number again");
+            else
+            {
+                Ellipse childCtrl = new Ellipse();
+                //  childCtrl.Name = "Elip"+value_input;
+                childCtrl.StrokeThickness = 2;
+                childCtrl.Stroke = Brushes.Black;
+                childCtrl.Width = 40;
+                childCtrl.Height = 40;
+                childCtrl.Opacity = 0.5;
+              childCtrl.Fill = System.Windows.Media.Brushes.Yellow;
+                
+
+                tree.Add(Convert.ToInt32(tb_insert.Text));
+                Label child = new Label();
+                child.Content = tb_insert.Text;
+                child.Foreground = System.Windows.Media.Brushes.Black;
+                child.HorizontalContentAlignment = HorizontalAlignment.Center;
+                
+
+                
+                mainCanvas.Children.Add(child);
+
+
+
+                if (count == 0)
+                {
+                    Canvas.SetTop(childCtrl, y);
+                    Canvas.SetLeft(childCtrl, x);
+
+                    Canvas.SetTop(child, y);
+                    Canvas.SetLeft(child, x);
+
+                }
+                else
+                {
+                    Node cur = tree.Find(Convert.ToInt32(tb_insert.Text));
+                    Canvas.SetTop(childCtrl, find_point(cur.flo, cur.pos).y);
+                    Canvas.SetLeft(childCtrl, find_point(cur.flo, cur.pos).x);
+
+                    Canvas.SetTop(child, find_point(cur.flo, cur.pos).y);
+                    Canvas.SetLeft(child, find_point(cur.flo, cur.pos).x);
+
+                    double pos_par = Math.Ceiling(cur.pos / 2.0);
+                    double x2 = find_point(cur.flo, cur.pos).x;
+                    double y2 = find_point(cur.flo, cur.pos).y;
+                   
+                    if (cur.flo - 1 == 0 && pos_par == 1)
+                    {
+                        addLine(484, 40, x2 + 20, y2 + 20);
+                        move(childCtrl, 464 - x2, 20 - y2, 0, 0, time);
+                        move1(child, 464 - x2, 20 - y2, 0, 0, time);
+                    }
+                    else
+                    {
+                        double x1 = find_point(cur.flo - 1, (int)pos_par).x;
+                        double y1 = find_point(cur.flo - 1, (int)pos_par).y;
+                        addLine(x1 + 20, y1 + 20, x2 + 20, y2 = 20);
+                        move(childCtrl, x1 - x2, y1 - y2, 0, 0, time);
+                        move1(child, x1 - x2, y1 - y2, 0, 0, time);
+                    }
+
+
+
+
+                }
+                mainCanvas.Children.Add(childCtrl);
+                count++;
+                mainCanvas.Opacity = 1;
+            
+            tb_insert.Clear();
+            }
+
+        }
+        public void move(Ellipse target,  double oldX, double oldY, double newX,
+       double newY, double time)
+        {
+            TranslateTransform trans = new TranslateTransform();
+        
+           target.RenderTransform = trans;
+            target.RenderTransformOrigin =  new Point(0, 0);
+            DoubleAnimation anim1 = new DoubleAnimation(oldY, newY,
+    TimeSpan.FromSeconds(time));
+         
+            trans.BeginAnimation(TranslateTransform.YProperty, anim1);
+         
+            DoubleAnimation anim2 = new DoubleAnimation(oldX, newX,
+    TimeSpan.FromSeconds(time));
+        
+            trans.BeginAnimation(TranslateTransform.XProperty, anim2);
+
+          
+        }
+        public void move1(Label target, double oldX, double oldY, double newX,
+      double newY, double time)
+        {
+            TranslateTransform trans = new TranslateTransform();
+            target.RenderTransform = trans;
+            target.RenderTransformOrigin = new Point(0, 0);
+            DoubleAnimation anim1 = new DoubleAnimation(oldY, newY,
+    TimeSpan.FromSeconds(time));
+
+            trans.BeginAnimation(TranslateTransform.YProperty, anim1);
+
+            DoubleAnimation anim2 = new DoubleAnimation(oldX, newX,
+    TimeSpan.FromSeconds(time));
+
+            trans.BeginAnimation(TranslateTransform.XProperty, anim2);
+
+        }
+
+    }
+}
---
+++ Navigation Drawer App/Red_BlackTree.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navigation_Drawer_App
+{
+    class Node
+    {
+        public Node left { get; set; }
+        public Node right { get; set; }
+
+        public Node parent { get; set; }
+        public int data { get; set; }
+        public Colour colour;
+        public int flo { get; set; }
+        public int pos { get; set; }
+        public Node(Colour colour) { this.colour = colour; }
+        public Node(int data, Colour colour) { this.data = data; this.colour = colour; }
+        public Node(int data)
+        {
+            this.data = data;
+            this.flo = 0;
+            this.pos = 1;
+            this.colour = Colour.Red;
+            this.right = this.left = this.parent = null;
+        }
+    }
+    enum Colour
+    {
+        Red,
+        Black
+    }
+    class Red_BlackTree
+    {
+        public Node root { get; set; }
+        public Red_BlackTree() {
+            this.root = null;
+        }
+        private void LeftRotate(Node X)
+        {
+            Node Y = X.right;
+            X.right = Y.left;
+            if (Y.left != null)
+            {
+                Y.left.parent = X;
+            }
+            if (Y != null)
+            {
+                Y.parent = X.parent;//link X's parent to Y
+            }
+            if (X.parent == null)
+            {
+                root = Y;
+            }
+            if (X == X.parent.left)
+            {
+                X.parent.left = Y;
+            }
+            else
+            {
+                X.parent.right = Y;
+            }
+            Y.left = X; //put X on Y's left
+            if (X != null)
+            {
+                X.parent = Y;
+            }
+
+        }
+        private void RightRotate(Node Y)
+        {
+            Node X = Y.left;
+            Y.left = X.right;
+            if (X.right != null)
+            {
+                X.right.parent = Y;
+            }
+            if (X != null)
+            {
+                X.parent = Y.parent;
+            }
+            if (Y.parent == null)
+            {
+                root = X;
+            }
+            if (Y == Y.parent.right)
+            {
+                Y.parent.right = X;
+            }
+            if (Y == Y.parent.left)
+            {
+                Y.parent.left = X;
+            }
+
+            X.right = Y;
+            if (Y != null)
+            {
+                Y.parent = X;
+            }
+        }
+        public Node Find(int key)
+        {
+            Button
+            bool isFound = false;
+            Node temp = root;
+            Node item = null;
+            while (!isFound)
+            {
+                if (temp == null)
+                {
+                    break;
+                }
+                if (key < temp.data)
+                {
+                    temp = temp.left;
+                }
+                if (key > temp.data)
+                {
+                    temp = temp.right;
+                }
+                if (key == temp.data)
+                {
+                    isFound = true;
+                    item = temp;
+                }
+            }
+            if (isFound)
+            {
+                return temp;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void Insert(int item)
+        {
+            Node newItem = new Node(item);
+            if (root == null)
+            {
+                root = newItem;
+                root.colour = Colour.Black;
+                return;
+            }
+            Node Y = null;
+            Node X = root;
+            while (X != null)
+            {
+                Y = X;
+                if (newItem.data < X.data)
+                {
+                    X = X.left;
+                }
+                else
+                {
+                    X = X.right;
+                }
+            }
+            newItem.parent = Y;
+            if (Y == null)
+            {
+                root = newItem;
+            }
+            else if (newItem.data < Y.data)
+            {
+                Y.left = newItem;
+            }
+            else
+            {
+                Y.right = newItem;
+            }
+            newItem.left = null;
+            newItem.right = null;
+            newItem.colour = Colour.Red;
+            InsertFixUp(newItem);
+        }
+        private void InOrderDisplay(Node current)
+        {
+            if (current != null)
+            {
+                InOrderDisplay(current.left);
+                Console.Write("({0}) ", current.data);
+                InOrderDisplay(current.right);
+            }
+        }
+        private void InsertFixUp(Node item)
+        {
+            //Checks Red-Black Tree properties
+            while (item != root && item.parent.colour == Colour.Red)
+            {
+                /*We have a violation*/
+                if (item.parent == item.parent.parent.left)
+                {
+                    Node Y = item.parent.parent.right;
+                    if (Y != null && Y.colour == Colour.Red)//Case 1: uncle is red
+                    {
+                        item.parent.colour = Colour.Black;
+                        Y.colour = Colour.Black;
+                        item.parent.parent.colour = Colour.Red;
+                        item = item.parent.parent;
+                    }
+                    else //Case 2: uncle is black
+                    {
+                        if (item == item.parent.right)
+                        {
+                            item = item.parent;
+                            LeftRotate(item);
+                        }
+                        //Case 3: recolour & rotate
+                        item.parent.colour = Colour.Black;
+                        item.parent.parent.colour = Colour.Red;
+                        RightRotate(item.parent.parent);
+                    }
+
+                }
+                else
+                {
+                    //mirror image of code above
+                    Node X = null;
+
+                    X = item.parent.parent.left;
+                    if (X != null && X.colour == Colour.Black)//Case 1
+                    {
+                        item.parent.colour = Colour.Red;
+                        X.colour = Colour.Red;
+                        item.parent.parent.colour = Colour.Black;
+                        item = item.parent.parent;
+                    }
+                    else //Case 2
+                    {
+                        if (item == item.parent.left)
+                        {
+                            item = item.parent;
+                            RightRotate(item);
+                        }
+                        //Case 3: recolour & rotate
+                        item.parent.colour = Colour.Black;
+                        item.parent.parent.colour = Colour.Red;
+                        LeftRotate(item.parent.parent);
+
+                    }
+
+                }
+                root.colour = Colour.Black;
+            }
+        }
+        public void Delete(int key)
+        {
+
+            Node item = Find(key);
+            Node X = null;
+            Node Y = null;
+
+            if (item == null)
+            {
+                Console.WriteLine("Nothing to delete!");
+                return;
+            }
+            if (item.left == null || item.right == null)
+            {
+                Y = item;
+            }
+            else
+            {
+                Y = TreeSuccessor(item);
+            }
+            if (Y.left != null)
+            {
+                X = Y.left;
+            }
+            else
+            {
+                X = Y.right;
+            }
+            if (X != null)
+            {
+                X.parent = Y;
+            }
+            if (Y.parent == null)
+            {
+                root = X;
+            }
+            else if (Y == Y.parent.left)
+            {
+                Y.parent.left = X;
+            }
+            else
+            {
+                Y.parent.left = X;
+            }
+            if (Y != item)
+            {
+                item.data = Y.data;
+            }
+            if (Y.colour == Colour.Black)
+            {
+                DeleteFixUp(X);
+            }
+
+        }
+        private void DeleteFixUp(Node X)
+        {
+
+            while (X != null && X != root && X.colour == Colour.Black)
+            {
+                if (X == X.parent.left)
+                {
+                    Node W = X.parent.right;
+                    if (W.colour == Colour.Red)
+                    {
+                        W.colour = Colour.Black; //case 1
+                        X.parent.colour = Colour.Red; //case 1
+                        LeftRotate(X.parent); //case 1
+                        W = X.parent.right; //case 1
+                    }
+                    if (W.left.colour == Colour.Black && W.right.colour == Colour.Black)
+                    {
+                        W.colour = Colour.Red; //case 2
+                        X = X.parent; //case 2
+                    }
+                    else if (W.right.colour == Colour.Black)
+                    {
+                        W.left.colour = Colour.Black; //case 3
+                        W.colour = Colour.Red; //case 3
+                        RightRotate(W); //case 3
+                        W = X.parent.right; //case 3
+                    }
+                    W.colour = X.parent.colour; //case 4
+                    X.parent.colour = Colour.Black; //case 4
+                    W.right.colour = Colour.Black; //case 4
+                    LeftRotate(X.parent); //case 4
+                    X = root; //case 4
+                }
+                else 
+                {
+                    Node W = X.parent.left;
+                    if (W.colour == Colour.Red)
+                    {
+                        W.colour = Colour.Black;
+                        X.parent.colour = Colour.Red;
+                        RightRotate(X.parent);
+                        W = X.parent.left;
+                    }
+                    if (W.right.colour == Colour.Black && W.left.colour == Colour.Black)
+                    {
+                        W.colour = Colour.Black;
+                        X = X.parent;
+                    }
+                    else if (W.left.colour == Colour.Black)
+                    {
+                        W.right.colour = Colour.Black;
+                        W.colour = Colour.Red;
+                        LeftRotate(W);
+                        W = X.parent.left;
+                    }
+                    W.colour = X.parent.colour;
+                    X.parent.colour = Colour.Black;
+                    W.left.colour = Colour.Black;
+                    RightRotate(X.parent);
+                    X = root;
+                }
+            }
+            if (X != null)
+                X.colour = Colour.Black;
+        }
+        private Node Minimum(Node X)
+        {
+            while (X.left.left != null)
+            {
+                X = X.left;
+            }
+            if (X.left.right != null)
+            {
+                X = X.left.right;
+            }
+            return X;
+        }
+        private Node TreeSuccessor(Node X)
+        {
+            if (X.left != null)
+            {
+                return Minimum(X);
+            }
+            else
+            {
+                Node Y = X.parent;
+                while (Y != null && X == Y.right)
+                {
+                    X = Y;
+                    Y = Y.parent;
+                }
+                return Y;
+            }
+        }
+    }
+}
